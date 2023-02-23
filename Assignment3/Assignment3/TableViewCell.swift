c9940e551a958cb03e6563211beaf5fc7bc636c7
LoneHustler <ankitsingh1901@gmail.com>
2023-02-22 21:52:10 -0800

    Assignment-3

---
+++ Assignment3/Assignment3/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Assignment3
+//
+//  Created by Ankit Singh on 2/22/23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageContainer: UIImageView!
+    
+    @IBOutlet weak var lblImage: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+ 
+    
+}
