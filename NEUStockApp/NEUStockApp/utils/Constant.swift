43c7829b0aab5fb0f2891acc18de4e7fe1cd4d40
LoneHustler <ankitsingh1901@gmail.com>
2023-03-18 17:26:43 -0700

    Midterm

---
+++ NEUStockApp/NEUStockApp/utils/Constant.swift
@@ -0,0 +1,9 @@
+//
+//  Constant.swift
+//  NEUStockApp
+//
+//  Created by Ankit Singh on 2/11/23.
+//
+
+import Foundation
+let apiKey = "23993f7777f689966e97ec2d35ccfd4d"
---
+++ Question2Midterm/Question2Midterm/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  Question2Midterm
+//
+//  Created by Ankit Singh on 3/18/23.
+//
+
+import Foundation
+
+let apiKey = "23993f7777f689966e97ec2d35ccfd4d"
+let baseURL = "https://financialmodelingprep.com/api/v3/profile/"
